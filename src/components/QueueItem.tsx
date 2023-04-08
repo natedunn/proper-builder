e01eb4e5515c4bab139d15bac2277712672fda24
Nate Dunn <hello@natedunn.net>
2023-04-07 20:36:56 -0600

    Make queue item design slightly better

--- src/components/QueueItem.tsx
+++ src/components/QueueItem.tsx
@@ -18,9 +18,10 @@ export const QueueItem = (props: QueueItemProps) => {
   //   }
   // };
   return (
-    <div class='flex items-center justify-between rounded-xl border border-zinc-700 bg-zinc-800 p-4'>
-      <div>{props.item.name}</div>
-      <div>{props.item.version}</div>
+    <div class='flex items-center justify-between gap-4 rounded-xl border border-zinc-700 bg-zinc-800 p-4'>
+      <div class='font-bold'>{props.item.name}</div>
+      <div class='ml-auto'>{props.item.version}</div>
+      <div class='opacity-25'>|</div>
       <div>{props.item.origin}</div>
     </div>
   );
