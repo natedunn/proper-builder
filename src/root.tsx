f787bc3be17c4b5ef482abdf3f51559e330a7865
Nate Dunn <hello@natedunn.net>
2023-04-10 17:46:42 -0600

    Add script imports

--- src/root.tsx
+++ src/root.tsx
@@ -1,5 +1,5 @@
 // @refresh reload
-import { Suspense } from "solid-js";
+import { Suspense } from 'solid-js';
 import {
   useLocation,
   A,
@@ -12,23 +12,27 @@ import {
   Routes,
   Scripts,
   Title,
-} from "solid-start";
-import "./root.css";
+} from 'solid-start';
+import './root.css';
 
 export default function Root() {
   const location = useLocation();
   const active = (path: string) =>
-    path == location.pathname
-      ? "border-sky-600"
-      : "border-transparent hover:border-sky-600";
+    path == location.pathname ? 'border-sky-600' : 'border-transparent hover:border-sky-600';
   return (
-    <Html lang="en">
+    <Html lang='en'>
       <Head>
         <Title>SolidStart - With TailwindCSS</Title>
-        <Meta charset="utf-8" />
-        <Meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta charset='utf-8' />
+        <Meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin='' />
+        <link
+          href='https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,300;0,400;0,700;1,400;1,700&display=swap'
+          rel='stylesheet'
+        />
       </Head>
-      <Body class="flex flex-col min-h-screen">
+      <Body class='flex min-h-screen flex-col'>
         <Suspense>
           <ErrorBoundary>
             <Routes>
