4fe16e3e01fb1a085e4742868cccbf9b7ef8036e
Nate Dunn <hello@natedunn.net>
2023-04-12 12:26:31 -0600

    Update titles

--- src/root.tsx
+++ src/root.tsx
@@ -22,7 +22,7 @@ export default function Root() {
   return (
     <Html lang='en'>
       <Head>
-        <Title>SolidStart - With TailwindCSS</Title>
+        <Title>Proper — Making dependencies easier</Title>
         <Meta charset='utf-8' />
         <Meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='preconnect' href='https://fonts.googleapis.com' />
--- src/routes/(baseLayout)/how.tsx
+++ src/routes/(baseLayout)/how.tsx
@@ -1,13 +1,15 @@
+import { Title } from 'solid-start';
 import { Heading } from '~/components/Heading';
 
 export default function HowPage() {
   return (
-    <div>
+    <>
+      <Title>Proper — How to use</Title>
       <div class='h-16' />
       <Heading tag='h1'>
         So how do you <br />
         use Proper?
       </Heading>
-    </div>
+    </>
   );
 }
