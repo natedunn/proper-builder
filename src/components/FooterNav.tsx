6421606923dcf61963524eff2bb7dac55a348886
Nate Dunn <hello@natedunn.net>
2023-04-15 22:40:59 -0600

    Update to accurate year

--- src/components/FooterNav.tsx
+++ src/components/FooterNav.tsx
@@ -2,7 +2,7 @@ export const FooterNav = () => {
   const currentYear = new Date().getFullYear();
   return (
     <div class='py-6 text-center'>
-      © 2022—{currentYear} Proper is made by{' '}
+      © 2019 — {currentYear} Proper is built by{' '}
       <a class='link underline' href='https://natedunn.net'>
         Nate Dunn
       </a>
