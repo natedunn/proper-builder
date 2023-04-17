b5c22b11024e67e3ee665951c1e4c41b31789ae2
Nate Dunn <hello@natedunn.net>
2023-04-17 13:03:57 -0600

    Update button styling

--- src/components/Queue.tsx
+++ src/components/Queue.tsx
@@ -121,12 +121,44 @@ export const Queue = () => {
         <div class='sticky bottom-4'>
           <div class='flex items-center justify-between gap-3 rounded-xl border border-zinc-700/75 bg-zinc-900/50 px-5 py-4 backdrop-blur-lg'>
             <div class='flex gap-3'>
-              <button class='button'>Download script</button>
-              <button class='button'>Export queue</button>
+              <button class='button'>
+                <span class='inline-block'>
+                  <svg
+                    class='h-4 w-4'
+                    viewBox='0 0 15 15'
+                    fill='none'
+                    xmlns='http://www.w3.org/2000/svg'
+                  >
+                    <path
+                      d='M7.50005 1.04999C7.74858 1.04999 7.95005 1.25146 7.95005 1.49999V8.41359L10.1819 6.18179C10.3576 6.00605 10.6425 6.00605 10.8182 6.18179C10.994 6.35753 10.994 6.64245 10.8182 6.81819L7.81825 9.81819C7.64251 9.99392 7.35759 9.99392 7.18185 9.81819L4.18185 6.81819C4.00611 6.64245 4.00611 6.35753 4.18185 6.18179C4.35759 6.00605 4.64251 6.00605 4.81825 6.18179L7.05005 8.41359V1.49999C7.05005 1.25146 7.25152 1.04999 7.50005 1.04999ZM2.5 10C2.77614 10 3 10.2239 3 10.5V12C3 12.5539 3.44565 13 3.99635 13H11.0012C11.5529 13 12 12.5528 12 12V10.5C12 10.2239 12.2239 10 12.5 10C12.7761 10 13 10.2239 13 10.5V12C13 13.1041 12.1062 14 11.0012 14H3.99635C2.89019 14 2 13.103 2 12V10.5C2 10.2239 2.22386 10 2.5 10Z'
+                      fill='currentColor'
+                      fill-rule='evenodd'
+                      clip-rule='evenodd'
+                    />
+                  </svg>
+                </span>
+                <span class='inline-block'>Download script</span>
+              </button>
+              {/* <button class='button'>Export queue</button> */}
             </div>
             <div>
-              <button class='button' onClick={handleClear}>
-                Clear all
+              <button class='button-secondary' onClick={handleClear}>
+                <span class='inline-block'>
+                  <svg
+                    class='h-4 w-4'
+                    viewBox='0 0 15 15'
+                    fill='none'
+                    xmlns='http://www.w3.org/2000/svg'
+                  >
+                    <path
+                      d='M5.5 1C5.22386 1 5 1.22386 5 1.5C5 1.77614 5.22386 2 5.5 2H9.5C9.77614 2 10 1.77614 10 1.5C10 1.22386 9.77614 1 9.5 1H5.5ZM3 3.5C3 3.22386 3.22386 3 3.5 3H5H10H11.5C11.7761 3 12 3.22386 12 3.5C12 3.77614 11.7761 4 11.5 4H11V12C11 12.5523 10.5523 13 10 13H5C4.44772 13 4 12.5523 4 12V4L3.5 4C3.22386 4 3 3.77614 3 3.5ZM5 4H10V12H5V4Z'
+                      fill='currentColor'
+                      fill-rule='evenodd'
+                      clip-rule='evenodd'
+                    />
+                  </svg>
+                </span>
+                <span class='inline-block'>Clear all</span>
               </button>
             </div>
           </div>
