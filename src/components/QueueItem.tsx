749bc6971e3d997e28ace6d676c6db8b52e40c44
Nate Dunn <hello@natedunn.net>
2023-04-16 22:51:33 -0600

    Add external link

--- src/components/QueueItem.tsx
+++ src/components/QueueItem.tsx
@@ -1,3 +1,4 @@
+import { Show } from 'solid-js';
 import { setQueue } from '~/lib/signals';
 import type { QueueItem as _QueueItem } from '~/lib/types';
 
@@ -20,7 +21,23 @@ export const QueueItem = (props: QueueItemProps) => {
       <div class='font-medium'>{props.item.name}</div>
       <div class='ml-auto text-sm'>{props.item.version}</div>
       <div class='opacity-10'>|</div>
-      {/* <div class='text-sm'>{props.item.origin}</div> */}
+      <Show when={!!props.item.url}>
+        <a
+          class='rounded bg-zinc-700 p-1 hocus:bg-amber-500 hocus:text-amber-950 hocus:outline-none'
+          href={props.item.url}
+          target='_blank'
+          rel='noreferrer'
+        >
+          <svg class='h-4 w-4' viewBox='0 0 15 15' fill='none' xmlns='http://www.w3.org/2000/svg'>
+            <path
+              d='M3 2C2.44772 2 2 2.44772 2 3V12C2 12.5523 2.44772 13 3 13H12C12.5523 13 13 12.5523 13 12V8.5C13 8.22386 12.7761 8 12.5 8C12.2239 8 12 8.22386 12 8.5V12H3V3L6.5 3C6.77614 3 7 2.77614 7 2.5C7 2.22386 6.77614 2 6.5 2H3ZM12.8536 2.14645C12.9015 2.19439 12.9377 2.24964 12.9621 2.30861C12.9861 2.36669 12.9996 2.4303 13 2.497L13 2.5V2.50049V5.5C13 5.77614 12.7761 6 12.5 6C12.2239 6 12 5.77614 12 5.5V3.70711L6.85355 8.85355C6.65829 9.04882 6.34171 9.04882 6.14645 8.85355C5.95118 8.65829 5.95118 8.34171 6.14645 8.14645L11.2929 3H9.5C9.22386 3 9 2.77614 9 2.5C9 2.22386 9.22386 2 9.5 2H12.4999H12.5C12.5678 2 12.6324 2.01349 12.6914 2.03794C12.7504 2.06234 12.8056 2.09851 12.8536 2.14645Z'
+              fill='currentColor'
+              fill-rule='evenodd'
+              clip-rule='evenodd'
+            />
+          </svg>
+        </a>
+      </Show>
       <button
         class='rounded bg-zinc-700 p-1 hocus:bg-red-900 hocus:outline-none'
         onClick={removeItem}
