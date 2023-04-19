1f331459b3a5602be72040673e564f15555320df
Nate Dunn <hello@natedunn.net>
2023-04-19 13:36:45 -0600

    Remove colophon for now

--- src/components/FooterNav.tsx
+++ src/components/FooterNav.tsx
@@ -5,24 +5,18 @@ export const FooterNav = () => {
   return (
     <div class=' border-t border-zinc-800 py-6 text-sm text-zinc-400'>
       <div class='container flex items-center justify-between space-x-2'>
-        <div class='flex items-center space-x-2'>
-          <div>
-            <A class='link' href='/colophon'>
-              Colophon
-            </A>
-          </div>
-          <div>|</div>
-          <div>
-            <a class='link' href='https://github.com/useproper/proper-app'>
-              Github
-            </a>
-          </div>
-        </div>
         <div>
           © 2019 — {currentYear} Proper is built by{' '}
           <a class='link' href='https://natedunn.net'>
             Nate Dunn
           </a>
+        </div>
+        <div class='flex items-center space-x-2'>
+          <div>
+            <a class='link' href='https://github.com/useproper/proper-app'>
+              Github
+            </a>
+          </div>
         </div>
       </div>
     </div>
